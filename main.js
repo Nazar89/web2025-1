6c8e2e523a940db238c6a99c6ca9bf1670561f4f
ghghg <nbolubas6@gmail.com>
2025-02-27 13:04:29 +0200

    b

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const githubUsername = "Nazar89"; // Замініть на ваше ім'я користувача GitHub
+const githubUsername = "Nazar899"; // Замініть на ваше ім'я користувача GitHub
 
 console.log(`Hello, ${githubUsername}!`);
